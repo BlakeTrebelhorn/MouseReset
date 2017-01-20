659a9f12ed000ec90da86ad297cdbc759085a79c
Blake Trebelhorn <blake.trebelhorn@gmail.com>
2017-01-20 13:47:36 -0600

    add comments to mouseReset

--- mouseReset.py
+++ mouseReset.py
@@ -3,25 +3,28 @@ import pyHook
 import pythoncom
 import time
 
-
+# get screen size, and find middle
 width = win32api.GetSystemMetrics(0)
 height = win32api.GetSystemMetrics(1)
 midWidth = (width + 1) / 2
 midHeight = (height + 1) / 2
 
 
+# move cursor to pixel x, y
 def moveCursor(x, y):
     # time.sleep(0.2)
     print('Moving mouse')
     win32api.SetCursorPos((x, y))
 
 
+# listen function for click event
 def onclick(event):
     print(event.Position)
-    moveCursor(int(midWidth), int(midHeight))
+    moveCursor(int(midWidth), int(midHeight))  # call moveCursor to screen mid
     return True
 
 
+# wait function - alternative to time.sleep()
 def wait(time_lapse):
     time_start = time.time()
     time_end = (time_start + time_lapse)
@@ -30,6 +33,8 @@ def wait(time_lapse):
         pass
 
 
+# function that checks if left or right control is pressed
+# if so, it unhooks the mouse and keyboard for 10 secs, then rehooks
 def checkCtrl(event):
     if event.Key in ('Lcontrol', 'Rcontrol'):
         print('Pausing for 10 Seconds')
@@ -43,8 +48,8 @@ def checkCtrl(event):
 
 try:
     hm = pyHook.HookManager()
-    hm.SubscribeMouseLeftUp(onclick)
-    hm.SubscribeKeyDown(checkCtrl)
+    hm.SubscribeMouseLeftUp(onclick)  # only watch left mouse button up
+    hm.SubscribeKeyDown(checkCtrl)  # only watch keyboard button down
     hm.HookMouse()
     hm.HookKeyboard()
     pythoncom.PumpMessages()
